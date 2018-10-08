9a7052331fdf0f806c4c6219c91c571bdd46f5c3
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-10-08 19:30:15 +0200

    Reformatted using two spaces

--- example.js
+++ example.js
@@ -1,18 +1,18 @@
 const Worker = require('./');
 
 const worker = new Worker(
-    async targets => {
-        console.log('Targets', targets);
-        return { raw: [], result: [] };
+  async targets => {
+    console.log('Targets', targets);
+    return { raw: [], result: [] };
+  },
+  {
+    engineAddress: 'http://localhost:8080',
+    topic: 'nmap_portscan',
+    workername: 'test',
+    async testScannerFunctionality() {
+      return { version: '1.5.3', testRun: 'successful' };
     },
-    {
-        engineAddress: 'http://localhost:8080',
-        topic: 'nmap_portscan',
-        workername: 'test',
-        async testScannerFunctionality() {
-            return { version: '1.5.3', testRun: 'successful' };
-        },
-    }
+  }
 );
 
 worker.startStatusServer();
